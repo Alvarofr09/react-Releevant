78889a87f9de2d081d56dfc19cedd7cf15c7f809
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-02-05 10:38:43 +0100

    importar todos los componentes y usarlos todos

--- prueba-vite/src/App.jsx
+++ prueba-vite/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Profile, ShoppingList } from "./main"; // Importa el componente Profile desde su ubicación
+
+import * as Componets from "./main"; // Importa el componente Profile desde su ubicación
 const container = document.getElementById("root");
 
 // Renderiza el componente Profile dentro del contenedor con id 'root'
 const root = createRoot(container);
 root.render(
 	<>
-		<Profile />
-		<ShoppingList />
+		<Componets.Profile />
+		<Componets.ShoppingList />
+		<Componets.MyButton />
 	</>
 );
